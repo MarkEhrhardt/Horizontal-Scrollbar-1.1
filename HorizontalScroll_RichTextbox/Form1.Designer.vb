64169584539fdcd45868f35ccc8bde7ae76b40b1
MarkEhrhardt <Mark@TheEhrhardts.com>
2022-01-23 17:02:41 -0600

    Init commit

---
+++ HorizontalScroll_RichTextbox/Form1.Designer.vb
@@ -0,0 +1,69 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.rtb1 = New System.Windows.Forms.RichTextBox()
+        Me.HScrollBar1 = New System.Windows.Forms.HScrollBar()
+        Me.SuspendLayout()
+        '
+        'rtb1
+        '
+        Me.rtb1.AcceptsTab = True
+        Me.rtb1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.rtb1.AutoWordSelection = True
+        Me.rtb1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rtb1.HideSelection = False
+        Me.rtb1.Location = New System.Drawing.Point(50, 12)
+        Me.rtb1.Name = "rtb1"
+        Me.rtb1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical
+        Me.rtb1.Size = New System.Drawing.Size(1431, 701)
+        Me.rtb1.TabIndex = 1
+        Me.rtb1.Text = resources.GetString("rtb1.Text")
+        Me.rtb1.WordWrap = False
+        '
+        'HScrollBar1
+        '
+        Me.HScrollBar1.Location = New System.Drawing.Point(-2, 716)
+        Me.HScrollBar1.Name = "HScrollBar1"
+        Me.HScrollBar1.Size = New System.Drawing.Size(1483, 17)
+        Me.HScrollBar1.TabIndex = 9
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1484, 761)
+        Me.Controls.Add(Me.HScrollBar1)
+        Me.Controls.Add(Me.rtb1)
+        Me.Name = "frmMain"
+        Me.Text = "frmMain"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents rtb1 As RichTextBox
+    Friend WithEvents HScrollBar1 As HScrollBar
+End Class
---
+++ HorizontalScroll_RichTextbox/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("HorizontalScroll_RichTextbox")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("HorizontalScroll_RichTextbox")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("ae6818d4-3af4-43e5-886a-8100832f806f")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
